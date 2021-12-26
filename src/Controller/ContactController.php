89beecfcaae08e9a2e43a8755824b8910bd59ac2
Vibedylan <dylanbourbotte62@icloud.com>
2021-12-26 14:22:17 +0100

    Création de mon formulaire et ajout sur ma page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Repository\DepartementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +14,17 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, DepartementRepository $departementRepository): Response
     {
+
+        $form = $this->createForm(ContactType::class);
+
+        $formView = $form->createView();
+
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'formView' => $formView
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,17 +2,44 @@
 
 namespace App\Form;
 
+use App\Entity\Departement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Tapez votre nom'],
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Tapez votre prénom'],
+            ])
+            ->add('mail', EmailType::class, [
+                'label' => 'Votre adresse e-mail',
+                'attr' => ['placeholder' => 'Tapez votre adresse email'],
+            ])
+            ->add('departement', EntityType::class, [
+                'label' => 'Département',
+                'placeholder' => '-- Choisir un département --',
+                'class' => Departement::class,
+                'choice_label' => function (Departement $departement) {
+                    return strtoupper($departement->getName());
+                }
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'attr' => ['placeholder' => 'Veuillez tapez votre message']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
