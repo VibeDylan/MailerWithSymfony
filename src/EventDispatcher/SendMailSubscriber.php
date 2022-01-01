36688006fa6a26db4397baade7c4a4cbdf9638c1
Vibedylan <dylanbourbotte62@icloud.com>
2022-01-01 19:42:28 +0100

    Refactorisation de mon SendMailSubscriber

--- src/EventDispatcher/SendMailSubscriber.php
+++ src/EventDispatcher/SendMailSubscriber.php
@@ -8,23 +8,29 @@ use App\Event\SendMailEvent;
 use App\Repository\DepartementRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 
 class SendMailSubscriber implements EventSubscriberInterface {
 
     private $departementRepository;
+    private $mailer;
+
     public function __construct(DepartementRepository $departementRepository, MailerInterface $mailer) {
         $this->departementRepository = $departementRepository;
         $this->mailer = $mailer;
     }
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             'send.mail' => 'sendMail'
         ];
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function sendMail(SendMailEvent $event) {
 
         $data = $event->getInfo();
